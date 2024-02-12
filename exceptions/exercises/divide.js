ae13fd6e80b937d5a3e39d2fe8f7ab822c7d6a66
Steven <dombrowskis153@yahoo.com>
2024-02-12 04:25:40 -0500

    Excercise done

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -1,7 +1,16 @@
 // Write a function called 'divide' that takes two parameters: a numerator and a denominator.
+let divide = function(numerator, denominator){
+
 
 // Your function should return the result of numerator / denominator.
 
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+if (denominator == 0){
+    throw Error("Attempted to devide by zero.")
+}
+else {return numerator / denominator}
+}
+
+console.log(divide(10, 0))
--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -3,6 +3,11 @@ function gradeLabs(labs) {
     let lab = labs[i];
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
+    try{
+      console.log("lab.runLab is not defined: ", runLab[i])
+    } catch(err){
+      console.log("Error Thrown");
+    } 
   }
 }
 
@@ -21,4 +26,28 @@ let studentLabs = [
   }
 ];
 
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+
 gradeLabs(studentLabs);
+
+gradeLabs(studentLabs2);
