db89931031a079fa2078bc563a1bbcffe9888041
Steven Dombrowski <dombrowskis153@yahoo.com>
2024-02-26 12:42:40 -0500

    DOM Exercise Done

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,23 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    let textChange = function(){
+        paragraph.innerHTML = "Houston, we have liftoff!"
+    }
     
+   button.addEventListener("click", textChange)
+   missionAbort.addEventListener('mouseover', function(){
+    missionAbort.style.backgroundColor = "red";
+   }) 
+   missionAbort.addEventListener('mouseleave', function(){
+    missionAbort.style.backgroundColor = "";  
+})
+   missionAbort.addEventListener("click", function(){
+    let result = confirm("Are you sure you want to abort the mission?")
+    if(result === true){
+        paragraph.innerHTML = "Mission aborted! Space shuttle returning home"
+    }
+   })
 }
 
 window.addEventListener("load", init);
