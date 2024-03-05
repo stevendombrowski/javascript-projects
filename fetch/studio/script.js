ba918b0c7180b00d17378670218455d8339cae16
Steven <dombrowskis153@yahoo.com>
2024-03-04 21:13:47 -0500

    studio done

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,29 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function(){
+    
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response){
+        response.json().then(function(json){
+            let container = document.getElementById('container')
+            let i = 0
+            let button = document.getElementById('button')
+            button.addEventListener('click', function(){
+               
+                container.innerHTML  += `
+                <div class = astronaut>
+                    <h3> ${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[i].hoursInSpace}
+                        <li>ID: ${json[i].id}</li>
+                        <li>Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills}</li>
+                    </ul>
+                    <img class=avatar src=${json[i].picture}>
+                </div>
+            `
+            i += 1;
+            
+            
+            })
+        })
+    })
+})
